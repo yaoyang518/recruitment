1d262f584f9b9b2a844e7cb1cda6878b1ce08bd5
yaoyang <yaoyang518@126.com>
2019-06-20 09:35:25 +0800

    初始化

---
+++ src/main/java/com/yaoyang/recruitment/RecruitmentApplication.java
@@ -0,0 +1,13 @@
+package com.yaoyang.recruitment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentApplication.class, args);
+	}
+
+}
